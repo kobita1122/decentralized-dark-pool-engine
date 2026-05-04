cfbf40bdf233eedf95570a6511cf5d8238f1c75c
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-05-04 23:03:36 +0600

    Create darkpool-constants.js

---
+++ darkpool-constants.js
@@ -0,0 +1,6 @@
+export const DARKPOOL_CONFIG = {
+    MATCHING_ENGINE_URL: "https://api.hidden-matches.io",
+    MIN_ORDER_SIZE_USD: 50000,
+    REVEAL_TIMEOUT_BLOCKS: 100,
+    PROTOCOL_FEE_BPS: 5
+};
